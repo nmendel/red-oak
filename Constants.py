52b29bc99c02574a037f09d33bfa8128fd79977d
Nick Mendel <nick.mendel@gmail.com>
2015-03-25 22:56:35 -0400

    Update constants to match csv file.

--- Constants.py
+++ Constants.py
@@ -4,7 +4,7 @@
 MUTATION_CONSTANT = 33
 MAX_PIZZA_THRESH = 1.0
 
-AGENT_HEADER_IGNORE = ['request_id', 'request_type', 'received_pizza']
+AGENT_HEADER_IGNORE = ['id', 'request_type', 'received_pizza']
 WEIGHT = 'weight'
 THRESH = 'thresh'
 PIZZA = 'pizza'
