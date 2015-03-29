4e52f2641c277fd03751aa7e66c7b76d7e3d6e25
Zach Curtis <zakurtis@gmail.com>
2015-03-29 17:25:59 -0400

    Added field to csv to mark data as training or testing data

--- requestscorer.py
+++ requestscorer.py
@@ -18,17 +18,30 @@ def print_text(dataset):
         input()
 
 def write_csv(dataset):
-    csvfile = open('output.csv', 'w')
+    count = 1
+    csvfile = open('scored_pizza_request_dataset.csv', 'w')
     swriter = csv.writer(csvfile)
-    swriter.writerow(['id', 'received_pizza', 'student', 'money', 'job', 'family', 'desire', 'num_words', 'account_age', 'length_of_time_on_raop'])
+    swriter.writerow(['id', 'received_pizza', 'student', 'money', 'job', 'family', 'desire', 'num_words', 'account_age', 'length_of_time_on_raop', 'request_type'])
     for datnum in dataset:
         reqI = RequestInfo(datnum)
         req = reqI.score_narrative()
+
+        #Set the request type. For every five data, one will be marked test, the other four will be marked train
+        if count%5 == 0 :
+            request_type = 'test'
+            count = 1
+        else:
+            request_type = 'train'
+            count += 1
+
+        #Write the data to the csv
         swriter.writerow([datnum['request_id'],
                           datnum['requester_received_pizza'], req['student'], req['money'], req['job'], req['family'], req['desire'],
                           reqI.score_request_length(),
                           reqI.score_requester_account_age_in_days_at_request(),
-                          reqI.score_requester_days_since_first_post_on_raop_at_request()])
+                          reqI.score_requester_days_since_first_post_on_raop_at_request(),
+                          request_type])
+
         
 if __name__ == '__main__':
     path = './pizza_request_dataset/pizza_request_dataset.json'
