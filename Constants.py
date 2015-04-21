15beb0967a33df5b5c4830b6090c480bd78e609f
Nick Mendel <nick.mendel@gmail.com>
2015-04-20 20:57:07 -0400

    Check in kaggle_test.csv, penalize agents with too many predictions of false.

--- Constants.py
+++ Constants.py
@@ -4,8 +4,12 @@
 MUTATION_CONSTANT = 5
 MAX_PIZZA_THRESH = 1.0
 
+# Penalize agents that guess false too often
+MAX_FALSE_PERCENTAGE = .85
+TOO_MANY_FALSE_PENALTY = .20
+
 # stop the loop if the best agent does this well
-SCORE_THRESHOLD = .85
+SCORE_THRESHOLD = .95
 
 AGENT_HEADER_IGNORE = ['id', 'received_pizza']
 WEIGHT = 'weight'
