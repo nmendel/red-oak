5d5f3631ddbc6c5262f65a8dbe31eaa142f6c1fa
Zach Curtis <zakurtis@gmail.com>
2015-04-22 02:40:08 -0400

    modified dataset_utils to drop a datum if it was a part of the dataset, don't have it working quite yet

--- pizza_request_dataset/dataset_utils.py
+++ pizza_request_dataset/dataset_utils.py
@@ -18,11 +18,13 @@ def create_kfolds(dataset):
         if not os.path.exists(dirname):
             os.makedirs(dirname)
         for datum in dataset:
-            if count % 5 == 0:
-                testdataset.append(datum)
-            else:
-                traindataset.append(datum)
-            count += 1
+            #line to ignore data in kaggle set
+            if '"in_test_set": true,' not in datum:
+                if count % 5 == 0:
+                    testdataset.append(datum)
+                else:
+                    traindataset.append(datum)
+                count += 1
         testfilename = r'./kfold%d/test_data_fold%d.json' % (x, x)
         with open(testfilename, 'w') as testfile:
             json.dump(testdataset, testfile, indent=4, separators=(',', ': '))
