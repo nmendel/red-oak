5d06a1f272931ef4bd92f7181062d40e457e2b0c
Nick Mendel <nick.mendel@gmail.com>
2015-04-23 23:30:48 -0400

    Add kaggle_test_with_answers.csv, change score_threshold constant.

--- Constants.py
+++ Constants.py
@@ -9,7 +9,9 @@ MAX_FALSE_PERCENTAGE = .85
 TOO_MANY_FALSE_PENALTY = .20
 
 # stop the loop if the best agent does this well
-SCORE_THRESHOLD = .95
+SCORE_THRESHOLD = .90
+
+#CHECK_AGAINST_TEST_AFTER_N_GENS = 10
 
 AGENT_HEADER_IGNORE = ['id', 'received_pizza']
 WEIGHT = 'weight'
